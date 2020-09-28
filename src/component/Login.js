cee930a1cd5d7301b9c34aa7f5bb4692a25ddd6b
rahuljha661 <rahuljha12527@gmail.com>
2020-09-28 22:34:54 +0530

    go to next page

--- src/component/Login.js
+++ src/component/Login.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
-import {BrowserRouter as Router,Link,Route} from 'react-router-dom';
-import WorldClock from './WorldClock';
+import { BrowserRouter as Router, Link, Route } from "react-router-dom";
+import WorldClock from "./WorldClock";
 
 class Login extends Component {
   constructor(props) {
@@ -9,6 +9,7 @@ class Login extends Component {
     this.state = {
       email: "",
       password: "",
+      loggedIn: false,
     };
   }
 
@@ -24,41 +25,47 @@ class Login extends Component {
     });
   };
 
-  handleFormSubmit=(e)=>{
-      e.preventDefault();
-      const {email , password}=this.state;
-      if(email==='rahuljha12527@gmail.com'  && password==='1234'){
-         <Router>
-             
-             <Route path="/login" component={WorldClock} />
-         </Router>   
-      }
-  }
+  handleFormSubmit = (e) => {
+    e.preventDefault();
+    const { email, password } = this.state;
+    if (email === "rahuljha12527@gmail.com" && password === "1234") {
+      this.setState({
+        loggedIn: true,
+      });
+    }
+  };
 
   render() {
+    const { loggedIn } = this.state;
     return (
-      <form className="login-form">
-        <span className="login-signup-header">Log In</span>
-        <div className="field">
-          <input
-            type="email"
-            placeholder="Email"
-            required
-            onChange={this.handleEmailChange}
-          />
-        </div>
-        <div className="field">
-          <input
-            type="password"
-            placeholder="Password"
-            required
-            onChange={this.handlePasswordChange}
-          />
-        </div>
-        <div className="field">
-          <button onClick={this.handleFormSubmit}>LogIn</button>
-        </div>
-      </form>
+      <>
+        {loggedIn ? (
+          <WorldClock />
+        ) : (
+          <form className="login-form">
+            <span className="login-signup-header">Log In</span>
+            <div className="field">
+              <input
+                type="email"
+                placeholder="Email"
+                required
+                onChange={this.handleEmailChange}
+              />
+            </div>
+            <div className="field">
+              <input
+                type="password"
+                placeholder="Password"
+                required
+                onChange={this.handlePasswordChange}
+              />
+            </div>
+            <div className="field">
+              <button onClick={this.handleFormSubmit}>LogIn</button>
+            </div>
+          </form>
+        )}
+      </>
     );
   }
 }
